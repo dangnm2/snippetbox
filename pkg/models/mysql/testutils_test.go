8f2aefa0c3ba39035260e59421fa163c09e828eb
dangngo <ngominh.dang96@gmail.com>
2019-10-29 11:33:30 +0700

    update mysql package using env vars for test db config

--- pkg/models/mysql/testutils_test.go
+++ pkg/models/mysql/testutils_test.go
@@ -2,12 +2,40 @@ package mysql
 
 import (
 	"database/sql"
+	"fmt"
 	"io/ioutil"
+	"os"
 	"testing"
 )
 
+type config struct {
+	user     string
+	password string
+	name     string
+	address  string
+}
+
+func getEnv(key string, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
+
+func getDBConfig() config {
+	return config{
+		name:     getEnv("SNIPPETBOX_DB_NAME", "snippetbox"),
+		user:     getEnv("SNIPPETBOX_DB_USER", "root"),
+		password: getEnv("SNIPPETBOX_DB_PASSWORD", ""),
+		address:  getEnv("SNIPPETBOX_DB_ADDRESS", "127.0.0.1"),
+	}
+}
+
 func newTestDB(t *testing.T) (*sql.DB, func()) {
-	db, err := sql.Open("mysql", "test_web:Test@1234@tcp(192.168.56.10:3306)/test_snippetbox?parseTime=true&multiStatements=true")
+	config := getDBConfig()
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true", config.user, config.password, config.address, config.name)
+
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		t.Fatal(err)
 	}
